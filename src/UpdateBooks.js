a8b6574e3a37f8a6cdd77a4d121b47797211f43b
Sokiyna <sokiynanaser@yahoo.com>
2021-06-02 20:06:26 +0300

    finish lab14

--- src/UpdateBooks.js
+++ src/UpdateBooks.js
@@ -7,7 +7,7 @@ import Form from 'react-bootstrap/Form';
 
 
 
-class BookFormModel extends React.Component {
+class UpdateBooks extends React.Component {
 
 
 
@@ -17,31 +17,31 @@ class BookFormModel extends React.Component {
             <>
 
 
-                <Modal show={this.props.showvalue}  >
-                    <Modal.Header closeButton onClick={this.props.hideValue} >
+                <Modal show={this.props.showUpdates} onHide={this.props.handleCloseUpdate} >
+                    <Modal.Header closeButton >
                         <Modal.Title>Modal heading</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
 
-                        <Form onSubmit={(e) => this.props.addBooks(e)}>
+                        <Form onSubmit={(e) => this.props.updateBooks(e)} >
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Label>Book Name </Form.Label>
-                                <Form.Control type="text" placeholder="Enter Book Name" name="BookName" onChange={(e) => this.props.updateBookName(e)} />
+                                <Form.Control type="text" placeholder="Enter Book Name" name="BookName" onChange={(e) => this.props.updateBookName(e)} value={this.props.name} />
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Label>Book discription </Form.Label>
-                                <Form.Control type="text" placeholder="Enter Book discription" name="BookDiscription" onChange={(e) => this.props.updateBookDiscription(e)} />
+                                <Form.Control type="text" placeholder="Enter Book discription" name="BookDiscription" onChange={(e) => this.props.updateBookDiscription(e)} value={this.props.discription} />
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Label>Book image Url</Form.Label>
-                                <Form.Control type="text" placeholder="Enter Book image Url" name="BookImageUrl" onChange={(e) => this.props.updateBookImageUrl(e)} />
+                                <Form.Control type="text" placeholder="Enter Book image Url" name="BookImageUrl" onChange={(e) => this.props.updateBookImageUrl(e)} value={this.props.imge} />
                             </Form.Group>
                             <Button variant="primary" type="submit">
                                 Submit
                              </Button>
-                            <Button variant="secondary" onClick={this.props.hideValue}>
+                            <Button variant="secondary" onClick={this.props.handleCloseUpdate}>
                                 Close
                              </Button>
 
@@ -57,4 +57,4 @@ class BookFormModel extends React.Component {
     }
 
 }
-export default BookFormModel;
+export default UpdateBooks;
